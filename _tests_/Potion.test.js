f5810c4d753595ca02261dfb27f438c1eebcccfa
Jessica Santiago <jessie_13084@yahoo.com>
2022-04-04 19:41:27 -0400

    on line lesson

---
+++ _tests_/Potion.test.js
@@ -0,0 +1,9 @@
+ const Potion = require('../lib/Potion');
+
+
+test('creates a health potion', () => {
+    const potion = new Potion('health');
+
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number))
+})
